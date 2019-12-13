95f1984c90e2de56dfbe73616872f6e762ab7a32
TS22082 <ts22082@gmail.com>
2019-12-13 13:50:12 -0800

    add secret keys

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
+require("dotenv").config();
+
+const url = `https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=`;
 
 router.get("/", (req, res) => {
-  console.log("this endpoint has been hit");
-  res.send("success");
+  axios.get(url + process.env.WEATHER_KEY).then(result => {
+    const data = result.data.main;
+    res.send(data);
+  });
 });
 
 module.exports = router;
